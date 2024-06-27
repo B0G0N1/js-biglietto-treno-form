3eaeaf479aafb8639c955b29e2ab2241e27eddef
B0G0N1 <michelebogoni2001@gmail.com>
2024-06-27 02:02:34 +0200

    [UPD] Add comment and change variables name

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
-function calcolaPrezzo() {
+// Funzione che parte premendo sul Bottone
+function price_calc() {
+    // Prezzo per chilometro
     let prezzoPerKm = 0.21;
 
-    let chilometri = document.getElementById('kilometri').value;
-    let eta = document.getElementById('eta').value;
+    // Prendi i valori degli input chilometri e età
+    let chilometri = document.getElementById('distance').value;
+    let age = document.getElementById('age').value;
 
+    // Calcola il prezzo base del biglietto e iniziallizzo sconto
     let prezzoBase = chilometri * prezzoPerKm;
     let sconto = 0;
 
+    // Applica lo sconto in base all'età
     if (eta < 18) {
-        sconto = 0.2;
+        sconto = 0.2; // Sconto del 20% per i minorenni
     } else if (eta > 65) {
-        sconto = 0.4;
+        sconto = 0.4; // Sconto del 40% per gli over 65
     }
 
+    // Calcola il prezzo finale applicando lo sconto
     let prezzoFinale = prezzoBase - (prezzoBase * sconto);
 
+    // Mostra il prezzo finale nella console
     console.log("Prezzo finale del biglietto: €" + prezzoFinale.toFixed(2));
 }
